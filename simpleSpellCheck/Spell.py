02f29b406eb1d5682c14d93651d7451161e0dc1b
John <jrbran+github@gmail.com>
2011-04-10 22:52:25 -0600

    A real commit message. Using some data, there are a few bugs. 'jjjooob' returns jab. and if no repeated letters exist then no vowel mutation is done.

--- simpleSpellCheck/Spell.py
+++ simpleSpellCheck/Spell.py
@@ -116,14 +116,16 @@ def find_word(word):
         for item,distance in possibles.iteritems():
             more_words = find_vowel_combinations(item,0,distance)
             total_words.update(more_words)
-        print total_words
+        #print total_words
         #Add to valid everything that is vowel mutated 
         for item,distance in total_words.iteritems():
             if item == find_exact(item):
                 valids[item] = total_words[item]
 
     if len(valids) > 0:
-        print sorted(valids, key=valids.get)
+        print valids
+        values = sorted(valids, key=valids.get)
+        value = values[0]
         
 
     output_str = "  Looking for a match for "+ word+ ": "
@@ -145,4 +147,11 @@ print find_word("matte")
 print find_word("mattte")
 print find_word("mmmattte")
 print find_word("CUNsperrICY")
+print find_word("weke")
+print find_word("jjoobbb")
+print find_word("inSIDE")
+print find_word("peepple")
+print find_word("sheeeeeep")
+print find_word("sheeple")
+
 
