c15131cc3502c05a21876cc7cb643320916bae58
John <jrbran+github@gmail.com>
2011-04-21 23:28:29 -0600

    Update that adds repeats on a per letter basis. Does not do all combinations of all repeats. This test shows the defeeat of repeated wrong vowels that the 'weeeke' test earlier showed.

--- simpleSpellCheck/Gen.py
+++ simpleSpellCheck/Gen.py
@@ -1,10 +1,15 @@
+'''
+    Will generate combinations of mutated vowels and then repeated characters to generate a mispelled word given a source word. Does not try to handle randomly capitlizing words since those all covered by a call to lower
+'''
+
 vowels = set(['a','e','i','o','u'])
 
 def generate(word):
     v_combins = [word]#Add original
     items = find_vowel_combinations(word,0,0)
     v_combins.extend(items)
-    return v_combins 
+    combinations = generate_repeats(v_combins)
+    return combinations
 
 def vowel_combin(word,i,x,distance):
     possibles = []
@@ -26,11 +31,33 @@ def find_vowel_combinations(word,start,distance):
             output.extend(items)
     return output
 
+def generate_repeats(list_of_words):
+    combins = []
+    combins.extend(list_of_words)
+    for word in list_of_words:
+        word_combs = []
+        for i in reversed(range(len(word))):
+            word_combs.extend(add_repeat(word,i))
+    combins.extend(word_combs)
+    return combins
+
+def add_repeat(word,i):
+    max_repeat = 4 #Max because I said so
+    repeats = [word]
+    while max_repeat > 0:
+        new_word = word[:i] + (max_repeat*word[i]) + word[i:]
+        repeats.append(new_word)
+        max_repeat -= 1
+    return repeats
+    
+    
+            
+
+
 words = [
         "weke",
         "awesomme"
         ]
-
 
 
 
