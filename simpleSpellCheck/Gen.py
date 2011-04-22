aa0e87ea6762b3997d626661c4306365a7bd0df7
John <jrbran+github@gmail.com>
2011-04-21 22:19:13 -0600

    Generate all the vowel combinations of a given word. Test against Spell via: python Gen.py | python Spell.py

--- simpleSpellCheck/Gen.py
+++ simpleSpellCheck/Gen.py
@@ -1,13 +1,40 @@
-
+vowels = set(['a','e','i','o','u'])
 
 def generate(word):
-    print word
+    v_combins = [word]#Add original
+    items = find_vowel_combinations(word,0,0)
+    v_combins.extend(items)
+    return v_combins 
 
+def vowel_combin(word,i,x,distance):
+    possibles = []
+    for vowel in vowels:
+        if vowel != x:
+            new_word = word[:i] + vowel + word[i+1:]
+            possibles.append(new_word  )
+            items = find_vowel_combinations(new_word,i+1,distance+1)
+            possibles.extend(items)
+    return possibles   
+
+def find_vowel_combinations(word,start,distance):
+    output = []
+    for i,x in enumerate(word):
+        if i < start:
+            continue
+        if x in vowels:
+            items = vowel_combin(word,i,x,distance)
+            output.extend(items)
+    return output
 
 words = [
         "weke",
         "awesomme"
         ]
 
+
+
+
 for word in words:
-    generate(word)
+    possibles = generate(word)
+    for item in possibles:
+        print item
